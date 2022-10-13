5dae502bd71270f58409411b100483ed354df203
Daniel-Ibarrola <dis96051@gmail.com>
2022-10-13 11:42:43 -0500

    Fixed bug extracting water when trying to extrac ligands

--- datasets/ligand-receptor/extract_ligands_to_pdb.py
+++ datasets/ligand-receptor/extract_ligands_to_pdb.py
@@ -3,29 +3,27 @@ import openpharmacophore.pharmacophore.pl_interactions as pli
 import os
 
 
-def save_ligand_to_pdb(traj, chain, lig_id, path):
+def save_ligand_to_pdb(traj, lig_id, path):
     """ Extract chains from a trajectory
 
         Parameters
         ----------
         traj: mdtraj.Trajectory
-
-        chain: int
-
         lig_id : str
-
         path: str
 
     """
+    ligand_name, _ = lig_id.split(":")
     ligand = traj.atom_slice(
-        [atom.index for atom in traj.topology.atoms if (atom.residue.chain.index == chain)]
+        [atom.index for atom in traj.topology.atoms if (atom.residue.name == ligand_name)]
     )
+    assert ligand.n_atoms > 0, f"{lig_id} failed to extract"
     file_name = "_".join(lig_id.split(":")) + "_chain.pdb"
     ligand.save_pdb(os.path.join(path, file_name))
 
 
 def extract_ligands():
-    """Extract the lignads in each pdb and save them to pdb files.
+    """Extract the ligands in each pdb and save them to pdb files.
     """
     for root, directories, filenames in os.walk("./test_cases"):
         for file in filenames:
@@ -33,6 +31,5 @@ def extract_ligands():
                 traj = mdt.load(os.path.join(root, file))
                 ligands = pli.find_ligands_in_traj(traj)
                 for lig in ligands:
-                    chain = pli.chain_names.index(lig.split(":")[-1])
-                    save_ligand_to_pdb(traj, chain, lig, root)
+                    save_ligand_to_pdb(traj, lig, root)
                     print(f"Created file for {lig}")
